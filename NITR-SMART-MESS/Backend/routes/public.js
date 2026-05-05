122fd5facad4e4e24d05b23d2519cf60521d5244
Bibeksah088 <194831731+Bibeksah088@users.noreply.github.com>
2026-05-05 07:08:58 +0530

    Delete NITR-SMART-MESS directory

--- NITR-SMART-MESS/Backend/routes/public.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMenu } = require('../controllers/publicController');
-
-router.get('/menu', getMenu);
-
-module.exports = router;
